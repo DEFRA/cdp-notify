6d0c40250b5bbc83520d9263c433441006a24e95
Ben Chidgey <ben@feedmypixel.com>
2024-11-19 14:39:04 +0000

    Updates to the date format wording

--- src/helpers/date/format-date.js
+++ src/helpers/date/format-date.js
@@ -1,6 +1,6 @@
 import { format } from 'date-fns'
 
-function formatDate(value, formatString = 'EEEE do MMMM yyyy, HH:mm:ss') {
+function formatDate(value, formatString = "EEEE do MMMM yyyy 'at' HH:mm:ss") {
   return format(value, formatString, { timeZone: 'UTC' })
 }
 

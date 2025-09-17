a263234d278ba6e6944fdafc2e18c5fe5bc1af25
Ben Chidgey <ben@feedmypixel.com>
2025-09-17 09:34:14 +0100

    CORE-1357: Move mongo to replicaSet

--- src/helpers/mongodb.js
+++ src/helpers/mongodb.js
@@ -1,32 +1,19 @@
 import { MongoClient } from 'mongodb'
 import { LockManager } from 'mongo-locks'
 
-import { config } from '~/src/config/index.js'
-
-/**
- * @satisfies { import('@hapi/hapi').ServerRegisterPluginObject<*> }
- */
 export const mongoDb = {
   plugin: {
     name: 'mongodb',
     version: '1.0.0',
-    /**
-     *
-     * @param { import('@hapi/hapi').Server } server
-     * @param {{mongoUrl: string, databaseName: string, retryWrites: boolean, readPreference: string}} options
-     * @returns {Promise<void>}
-     */
     register: async function (server, options) {
       server.logger.info('Setting up mongodb')
 
       const client = await MongoClient.connect(options.mongoUrl, {
-        retryWrites: options.retryWrites,
-        readPreference: options.readPreference,
-        // @ts-expect-error TS2339
+        ...options.mongoOptions,
         ...(server.secureContext && { secureContext: server.secureContext })
       })
 
-      const databaseName = options.databaseName
+      const { databaseName } = options
       const db = client.db(databaseName)
       const locker = new LockManager(db.collection('mongo-locks'))
 
@@ -34,42 +21,26 @@ export const mongoDb = {
 
       server.logger.info(`mongodb connected to ${databaseName}`)
 
-      // @ts-expect-error TS2769
       server.decorate('server', 'mongoClient', client)
-      // @ts-expect-error TS2769
-      server.decorate('server', 'db', db)
-      // @ts-expect-error TS2769
-      server.decorate('server', 'locker', locker)
-      server.decorate('request', 'db', () => db, { apply: true })
-      server.decorate('request', 'locker', () => locker, { apply: true })
+      server.decorate('request', 'mongoClient', client)
 
-      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      server.decorate('server', 'db', db)
+      server.decorate('request', 'db', db)
+
+      server.decorate('server', 'locker', locker)
+      server.decorate('request', 'locker', locker)
+
       server.events.on('stop', async () => {
         server.logger.info('Closing Mongo client')
         await client.close(true)
       })
     }
-  },
-  options: {
-    mongoUrl: config.get('mongoUri'),
-    databaseName: config.get('mongoDatabase'),
-    retryWrites: false,
-    readPreference: 'secondary'
   }
 }
 
-/**
- * @param {import('mongodb').Db} db
- * @returns {Promise<void>}
- */
 async function createIndexes(db) {
   await db.collection('mongo-locks').createIndex({ id: 1 })
 
   // Example of how to create a mongodb index. Remove as required
-  await db.collection('example-data').createIndex({ id: 1 })
+  return db.collection('example-data').createIndex({ id: 1 })
 }
-
-/**
- * To be mixed in with Request|Server to provide the db decorator
- * @typedef {{db: import('mongodb').Db, locker: import('mongo-locks').LockManager }} MongoDBPlugin
- */
